cc8f9dff8278ca64386dffd2fc639268b1f8729f
Debdutta Panda <me@debduttapanda.com>
2022-08-05 08:37:51 +0530

    Initial Commit

---
+++ sweater_shop/sweater_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweater_shopApp.swift
+//  sweater_shop
+//
+//  Created by Debdutta Panda on 05/08/22.
+//
+
+import SwiftUI
+
+@main
+struct sweater_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sweater_shopUITests/sweater_shopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sweater_shopUITestsLaunchTests.swift
+//  sweater_shopUITests
+//
+//  Created by Debdutta Panda on 05/08/22.
+//
+
+import XCTest
+
+class sweater_shopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
